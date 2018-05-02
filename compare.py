3833e8ce0ceab8ddeed972a76ffae7c6b41d65d4
hadware <hadware@gmail.com>
2018-05-03 01:52:43 +0200

    Fixing the compare.py script

--- compare.py
+++ compare.py
@@ -4,21 +4,27 @@ import argparse
 from os import path as osp
 from tabulate import tabulate
 
-def load_max_logs(dir_logs, nb_epochs=-1):
+def load_max_logs(dir_logs,metrics, nb_epochs=-1):
     path_logs = osp.join(dir_logs, 'logs.json')
     path_val_oe = osp.join(dir_logs, 'logs_eval_val_oe.json')
     logs = json.load(open(path_logs))
     out = {}
     epochs = logs["eval_epoch.epoch"]
     for k,v in logs.items():
-        if 'eval_epoch' in k:
-            argmax = np.argmax(v[:nb_epochs]) if nb_epochs != -1 else np.argmax(v)
-            out[k] = epochs[argmax], v[argmax]
+        if k in metrics:
+            if metrics[k] == "max":
+                argsup = np.argmax(v[:nb_epochs]) if nb_epochs != -1 else np.argmax(v)
+            elif metrics[k] == "min":
+                argsup = np.argmin(v[:nb_epochs]) if nb_epochs != -1 else np.argmin(v)
+            out[k] = epochs[argsup], v[argsup]
     if osp.isfile(path_val_oe):
         val_oe = json.load(open(path_val_oe))
         for k,v in val_oe.items():
-            argmax = np.argmax(v[:nb_epochs]) if nb_epochs != -1 else np.argmax(v)
-            out[k] = epochs[argmax], v[argmax]
+            if metrics[k] == "max":
+                argsup = np.argmax(v[:nb_epochs]) if nb_epochs != -1 else np.argmax(v)
+            elif metrics[k] == "min":
+                argsup = np.argmin(v[:nb_epochs]) if nb_epochs != -1 else np.argmin(v)
+            out[k] = epochs[argsup], v[argsup]
     return out
 
 
@@ -30,7 +36,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--nb_epochs', default=-1, type=int)
     parser.add_argument('--dir_logs', default='', type=str, nargs='*')
-    parser.add_argument('-k', '--keys', type=str, action='append', nargs='2',
+    parser.add_argument('-k', '--keys', type=str, action='append', nargs=2,
+                        metavar=('metric', 'order'),
                         default=[['eval_epoch.epoch', 'max'],
                                  ['eval_epoch.accuracy_top1', 'max'],
                                  ['eval_epoch.overall', 'max'],
@@ -49,23 +56,22 @@ if __name__ == '__main__':
             raise ValueError(raw)
         dir_logs[key] = path
 
-    keys = {key: min_or_max for key, min_or_max in args.keys}
+    metrics = {key: min_or_max for key, min_or_max in args.keys}
 
     logs = {}
     for log_name in dir_logs.keys():
-        logs[log_name] = load_max_logs(dir_logs[log_name], nb_epochs=args.nb_epochs)
+        logs[log_name] = load_max_logs(dir_logs[log_name], metrics, nb_epochs=args.nb_epochs)
 
-    for key in keys:
+    for key in metrics:
         names = []
         values = []
         epochs = []
         for name, log_values in logs.items():
-            if key in log_values:
-                names.append(name)
-                epoch, value = log_values[key]
-                epochs.append(epoch)
-                values.append(value)
-        values_names = sorted(zip(values,names, epochs),reverse=True)
+            names.append(name)
+            epoch, value = log_values[key]
+            epochs.append(epoch)
+            values.append(value)
+        values_names = sorted(zip(values, names, epochs),reverse=True)
         values_names = [[i+1, name, value] for i, (value, name) in enumerate(values_names)]
         print('\n\n## {}\n'.format(key))
         print(tabulate(values_names, headers=['Place', 'Method', 'Score', 'Epoch']))
