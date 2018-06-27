a5be6b6ff47491bd5f62e924a913a869ff8b446c
Remi Cadene <rcadene@lnode-au15-28.cm.cluster>
2018-06-26 20:02:05 -0700

    v0.0.5

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'torch>=0.4',
+        'torch',
         'numpy',
         'argparse',
         'pyyaml',
