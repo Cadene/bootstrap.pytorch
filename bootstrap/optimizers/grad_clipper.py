c7854c8abbb1428dc5bcd38e1f22a489de62bd44
durandtibo <thibaut.p.durand@borealisai.com>
2019-08-13 14:27:55 -0700

    update grad_clipper to use the clip_grad_norm_ if it is a recent version of pytorch

--- bootstrap/optimizers/grad_clipper.py
+++ bootstrap/optimizers/grad_clipper.py
@@ -1,4 +1,4 @@
-from torch.nn.utils.clip_grad import clip_grad_norm
+import torch
 
 class GradClipper():
 
@@ -6,12 +6,17 @@ class GradClipper():
         self.optimizer = optimizer
         self.grad_clip = grad_clip
 
+        if hasattr(torch.nn.utils, 'clip_grad_norm_'):
+            self.clip_grad_norm = torch.nn.utils.clip_grad_norm_
+        else:
+            self.clip_grad_norm =  torch.nn.utils.clip_grad.clip_grad_norm
+
     def step(self):
         params = []
         for group in self.optimizer.param_groups:
             for p in group['params']:
                 params.append(p)
-        clip_grad_norm(params, self.grad_clip)
+        self.clip_grad_norm(params, self.grad_clip)
         self.optimizer.step()
 
     def zero_grad(self):
