e266a9e3cfc2456207b2467ae12fd47d810f5031
durandtibo <thibaut.p.durand@borealisai.com>
2019-09-11 18:51:42 -0700

    update criterions factory + add tests

--- bootstrap/models/criterions/factory.py
+++ bootstrap/models/criterions/factory.py
@@ -1,35 +1,32 @@
 import importlib
 
-from bootstrap.lib.options import Options
 from bootstrap.lib.logger import Logger
-
-from .nll import NLLLoss
+from bootstrap.lib.options import Options
 from .bce import BCEWithLogitsLoss
 from .cross_entropy import CrossEntropyLoss
-from .cross_entropy import CrossEntropyLoss
+from .nll import NLLLoss
+
 
 def factory(engine=None, mode=None):
-
     if 'criterion' not in Options()['model'] or Options()['model']['criterion'] is None:
         return None
 
-    Logger()('Creating criterion for {} mode...'.format(mode))
+    Logger()(f'Creating criterion for {mode} mode...')
 
     if Options()['model']['criterion'].get('import', False):
         module = importlib.import_module(Options()['model']['criterion']['import'])
         criterion = module.factory(engine, mode)
 
     elif Options()['model']['criterion']['name'] == 'nll':
-        criterion = NLLLoss()
+        criterion = NLLLoss(**Options()['model']['criterion'])
 
     elif Options()['model']['criterion']['name'] == 'cross_entropy':
-        criterion = CrossEntropyLoss()
+        criterion = CrossEntropyLoss(**Options()['model']['criterion'])
 
     elif Options()['model']['criterion']['name'] == 'BCEWithLogitsLoss':
-        criterion = BCEWithLogitsLoss()
+        criterion = BCEWithLogitsLoss(**Options()['model']['criterion'])
 
     else:
         raise ValueError()
 
     return criterion
-
---
+++ tests/model/criterions/test_factory.py
@@ -0,0 +1,97 @@
+import pytest
+
+from bootstrap.lib.options import Options
+from bootstrap.models.criterions.bce import BCEWithLogitsLoss
+from bootstrap.models.criterions.cross_entropy import CrossEntropyLoss
+from bootstrap.models.criterions.factory import factory
+from bootstrap.models.criterions.nll import NLLLoss
+
+
+def test_initialize_options():
+    options = {
+        'model': {
+            'criterion': {},
+        },
+    }
+    Options(options, run_parser=False)
+
+
+def test_factory_incorrect_name():
+    Options()['model']['criterion'] = {'name': 'name'}
+    with pytest.raises(ValueError):
+        factory()
+
+
+def test_factory_incorrect_option_1():
+    Options()['model'] = {}
+    criterion = factory()
+    assert criterion is None
+
+
+def test_factory_incorrect_option_2():
+    Options()['model']['criterion'] = None
+    criterion = factory()
+    assert criterion is None
+
+
+def test_factory_nll_1():
+    Options()['model']['criterion'] = {'name': 'nll'}
+    criterion = factory()
+    assert isinstance(criterion, NLLLoss)
+
+
+def test_factory_nll_2():
+    Options()['model']['criterion'] = {
+        'name': 'nll',
+        'target_field_name': 'target',
+        'input_field_name': 'input',
+        'output_field_name': 'nll_loss',
+        'useless_option': 123,
+    }
+    criterion = factory()
+    assert isinstance(criterion, NLLLoss)
+    assert criterion.input_field_name == 'input'
+    assert criterion.output_field_name == 'nll_loss'
+    assert criterion.target_field_name == 'target'
+
+
+def test_factory_bce_with_logits_1():
+    Options()['model']['criterion'] = {'name': 'BCEWithLogitsLoss'}
+    criterion = factory()
+    assert isinstance(criterion, BCEWithLogitsLoss)
+
+
+def test_factory_bce_with_logits_2():
+    Options()['model']['criterion'] = {
+        'name': 'BCEWithLogitsLoss',
+        'target_field_name': 'target',
+        'input_field_name': 'input',
+        'output_field_name': 'nll_loss',
+        'useless_option': 123,
+    }
+    criterion = factory()
+    assert isinstance(criterion, BCEWithLogitsLoss)
+    assert criterion.input_field_name == 'input'
+    assert criterion.output_field_name == 'nll_loss'
+    assert criterion.target_field_name == 'target'
+
+
+def test_factory_cross_entropy_1():
+    Options()['model']['criterion'] = {'name': 'cross_entropy'}
+    criterion = factory()
+    assert isinstance(criterion, CrossEntropyLoss)
+
+
+def test_factory_cross_entropy_2():
+    Options()['model']['criterion'] = {
+        'name': 'cross_entropy',
+        'target_field_name': 'target',
+        'input_field_name': 'input',
+        'output_field_name': 'nll_loss',
+        'useless_option': 123,
+    }
+    criterion = factory()
+    assert isinstance(criterion, CrossEntropyLoss)
+    assert criterion.input_field_name == 'input'
+    assert criterion.output_field_name == 'nll_loss'
+    assert criterion.target_field_name == 'target'
