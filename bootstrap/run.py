def120b308db4853bad3faae6e4419c6d75ed4a6
Jean Bégaint <jean@jbegaint.fr>
2020-04-04 14:56:29 -0700

    Replace  os.system() calls

--- bootstrap/run.py
+++ bootstrap/run.py
@@ -1,6 +1,7 @@
 import io
 import os
 import sys
+import shutil
 import click
 import torch
 import traceback
@@ -19,13 +20,13 @@ from . import views
 def init_experiment_directory(exp_dir, resume=None):
     # create the experiment directory
     if not os.path.isdir(exp_dir):
-        os.system('mkdir -p ' + exp_dir)
+        os.makedirs(exp_dir)
     else:
         if resume is None:
             if click.confirm('Exp directory already exists in {}. Erase?'
                     .format(exp_dir, default=False)):
-                os.system('rm -r ' + exp_dir)
-                os.system('mkdir -p ' + exp_dir)
+                shutil.rmtree(exp_dir)
+                os.makedirs(exp_dir)
             else:
                 os._exit(1)
 
