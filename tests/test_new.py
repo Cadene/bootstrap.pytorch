eefd7e9c6dc37c8f4b3bf6dd6be9c2d7e02fc1c8
Micael Carvalho <mcoaky@gmail.com>
2020-05-17 14:03:24 -0700

    Fix tests

--- tests/test_new.py
+++ tests/test_new.py
@@ -1,13 +1,16 @@
 import os
-import pytest
+import sys
+from bootstrap.new import new_project
+from bootstrap.run import run
+
 
 def test_new(tmpdir):
-    exit_status = os.system(f'python -m bootstrap.new --project_name MyProject --project_dir {tmpdir}')
-    assert exit_status == 0
-
-    os.chdir(os.path.join(tmpdir, 'myproject.bootstrap.pytorch'))
-    exit_status = os.system('python -m bootstrap.run -o myproject/options/myproject.yaml --exp.dir logs/myproject/1_exp --misc.cuda False --engine.nb_epochs 10')
-    assert exit_status == 0
+    new_project('MyProject', tmpdir)
+    base_dir = os.path.join(tmpdir, 'myproject.bootstrap.pytorch')
+    test_dir = os.path.join(base_dir, 'myproject')
+    options_file = os.path.join(test_dir, 'options/myproject.yaml')
+    sys.path.insert(0, base_dir)
+    run(options_file, run_parser=False)
 
     fnames = [
         'ckpt_best_accuracy_engine.pth.tar',
@@ -26,4 +29,5 @@ def test_new(tmpdir):
     ]
 
     for fname in fnames:
-        assert os.path.isfile(f'logs/myproject/1_exp/{fname}')
+        file_path = os.path.join(test_dir, f'logs/myproject/1_exp/{fname}')
+        assert os.path.isfile(file_path)
